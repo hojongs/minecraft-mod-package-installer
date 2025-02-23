47bba7cab11bb9be7984a2e4183bf72b0338aaff
Jongho Jeon <hojong.jjh@gmail.com>
2025-02-23 16:33:10 +0900

    fix: Set --name option

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env sh
 
-pyinstaller --onefile --add-data "mods/*.jar:mods/" --manifest=minecraft-mod-installer main.py
+pyinstaller --onefile --add-data "mods/*.jar:mods/" --name=minecraft-mod-installer main.py
+
