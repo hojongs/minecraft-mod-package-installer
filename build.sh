915d7af3732bf4517d700654f99707a90d4a0b16
Jongho Jeon <hojong.jjh@gmail.com>
2025-03-05 01:12:40 +0900

    fix: macOS 에서 실행할 수 없는 문제
    - .app fileExt 를 설정하여 실행 파일로 인식하도록 함
    - universal build 하여 x86, arm64 모두 실행 가능하도록 함
      - ref: https://pyinstaller.org/en/stable/feature-notes.html#macos-multi-arch-support
    - Set `set -eu` to prevent error

--- build.sh
+++ build.sh
@@ -1,14 +1,25 @@
 #!/usr/bin/env bash
 
-# Default distribution if not provided
-distribution="${1:os-SNAPSHOT}"
+set -eu
 
-# Print the distribution to verify
+# Default distribution if not provided
+distribution="${1:-os-SNAPSHOT}"
+
 echo "Building distribution: $distribution"
 
-# Run pyinstaller to build the project with the distribution
+# Check if distribution contains 'Windows'
+if [[ "$distribution" == *Windows* ]]; then
+    fileExt="exe"
+    opt=""
+else
+    fileExt="app"
+    opt="--target-architecture universal2"
+fi
+
+# Universal build for different platforms
 pyinstaller \
   --onefile \
   --add-data "mods/*.jar:mods/" \
-  --name="minecraft-mod-package-installer-$distribution" \
+  --name="minecraft-mod-package-installer-$distribution.$fileExt" \
+  $opt \
   main.py
